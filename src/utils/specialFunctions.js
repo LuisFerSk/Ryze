1782984b5f4e850db9a024f8653b25026276c4a9
Luis Fernando <39956205+luisfersk@users.noreply.github.com>
2021-10-02 13:09:46 -0500

    Validar correos

--- src/utils/specialFunctions.js
+++ src/utils/specialFunctions.js
@@ -12,3 +12,6 @@ export const createAccordion = (title, icon, content, subtitle = "") => {
 export const createTab = (nameTab, iconTab, content) => {
 	return { nameTab, iconTab, content };
 };
+
+export const validarCorreos = (email) => /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(email);
+
