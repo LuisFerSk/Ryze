564005e988065b53a2e02576be113a1c8ede138c
Luis Fernando <39956205+luisfersk@users.noreply.github.com>
2021-10-02 09:36:00 -0500

    Providers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Router from './routes';
 import ThemeConfig from './theme';
+import Providers from "./Providers";
 import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
-    <ThemeConfig>
-      <ScrollToTop />
-      <Router />
-    </ThemeConfig>
+    <Providers>
+      <ThemeConfig>
+        <ScrollToTop />
+        <Router />
+      </ThemeConfig>
+    </Providers>
   );
 }
