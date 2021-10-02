579dea6d923250ac1750666a953aec2c5811bd9b
Luis Fernando <39956205+luisfersk@users.noreply.github.com>
2021-10-02 13:09:59 -0500

    componente mensajes

---
+++ src/components/shared/Mensaje.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import Alert from "@material-ui/lab/Alert";
+
+const ControlMensaje = () => {
+    const [mensaje, setMensaje] = useState();
+
+    const updateMensaje = (severity, label) => {
+        if (severity && label) {
+            setMensaje(
+                <Alert severity={severity}>
+                    {label}
+                </Alert>
+            )
+            return;
+        }
+
+        setMensaje();
+    }
+
+    return [mensaje, updateMensaje];
+}
+
+export default ControlMensaje;
