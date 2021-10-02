26f7755d9f6ea871fca9ae8fbba2d445693ce1cc
Luis Fernando <39956205+luisfersk@users.noreply.github.com>
2021-10-02 09:36:08 -0500

    Routes

--- src/routes.js
+++ src/routes.js
@@ -38,11 +38,10 @@ export default function Router() {
       children: [
         { path: 'login', element: <Login /> },
         { path: 'register', element: <Register /> },
-        { path: '404', element: <NotFound /> },
         { path: '/', element: <Navigate to="/dashboard" /> },
-        { path: '*', element: <Navigate to="/404" /> }
+        { path: '*', element: <NotFound /> }
       ]
     },
-    { path: '*', element: <Navigate to="/404" replace /> }
+    { path: '*', element: <NotFound /> }
   ]);
 }
