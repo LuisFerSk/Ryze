4b967330c23a6188ea76036baaae1ea0170be77d
LuisFerSk <lcampomontero@gmail.com>
2021-10-13 07:05:18 -0500

    new config firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
 	apiKey: "AIzaSyA-XRTS81NvnN0wTDba5tbsOpeyVp3tYgo",
@@ -8,8 +8,8 @@ const firebaseConfig = {
 	messagingSenderId: "47095546304",
 	appId: "1:47095546304:web:faa09f7837059d1fd6c825",
 	measurementId: "G-3Z0LL2NE50"
-  };
+};
 
-const fire = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export default fire;
+export default app;
